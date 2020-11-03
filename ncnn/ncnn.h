71034e4df2c8b080198f81e0767be590ee728b2b
axmadjon <axmadjon.xamidov@gmail.com>
2020-11-03 17:03:30 +0500

    Initial Commit

---
+++ ncnn/ncnn.h
@@ -0,0 +1,18 @@
+//
+//  ncnn.h
+//  ncnn
+//
+//  Created by MacBook Air GWS on 11/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ncnn.
+FOUNDATION_EXPORT double ncnnVersionNumber;
+
+//! Project version string for ncnn.
+FOUNDATION_EXPORT const unsigned char ncnnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ncnn/PublicHeader.h>
+
+
---
+++ ncnnTests/ncnnTests.m
@@ -0,0 +1,36 @@
+//
+//  ncnnTests.m
+//  ncnnTests
+//
+//  Created by MacBook Air GWS on 11/3/20.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ncnnTests : XCTestCase
+
+@end
+
+@implementation ncnnTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
